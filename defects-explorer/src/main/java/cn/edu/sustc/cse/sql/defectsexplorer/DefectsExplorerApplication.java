f64180a6b12217fb919a038b3a2fc3c73df51b09
Henrycobaltech <henrytech@outlook.com>
2018-10-29 09:05:37 +0800

    created project

---
+++ defects-explorer/src/main/java/cn/edu/sustc/cse/sql/defectsexplorer/DefectsExplorerApplication.java
@@ -0,0 +1,12 @@
+package cn.edu.sustc.cse.sql.defectsexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefectsExplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DefectsExplorerApplication.class, args);
+    }
+}
